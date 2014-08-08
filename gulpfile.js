6f6c28789afb23542671f67cf33273ce6acd11dc
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-08-08 21:22:13 +0200

    Added gulp covage and lint commands.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var istanbul = require('gulp-istanbul');
 var exit = require('gulp-exit');
 
 // Help module
@@ -13,4 +15,23 @@ gulp.task('test', 'Run the application tests', function () {
             reporter: 'spec'
         }))
         .pipe(exit());
+});
+
+gulp.task('coverage', 'Create istanbul code coverage report form tests',
+            function () {
+    gulp.src(['lib/**/*.js'])
+        .pipe(istanbul())
+        .on('finish', function () {
+            require('must');
+            gulp.src(['./examples/**/test.js'])
+                .pipe(mocha())
+                .pipe(istanbul.writeReports())
+                .pipe(exit());
+        });
+});
+
+gulp.task('lint', 'Execute JSHint checks on the code', function () {
+    gulp.src(['lib/**/*.js', 'examples/**/*.js'])
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
 });
